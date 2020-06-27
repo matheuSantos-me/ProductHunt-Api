e533625ae01d52fd4219bd53e276877fd319139b
Matheus Santos <matheus.santos@server.signainfo.com.br>
2020-06-26 22:18:57 -0300

    fix: Path of routes

--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,8 @@ const ProductController = require('./controllers/ProductController')
 
 routes.get('/products', ProductController.index)
 routes.get('/products/:id', ProductController.show)
-routes.post('products', ProductController.store)
+routes.post('/products', ProductController.store)
 routes.put('/products/:id', ProductController.update)
-routes.delete('products/:id', ProductController.destroy)
+routes.delete('/products/:id', ProductController.destroy)
 
 module.exports = routes
